3b4b12535eb0180eddd7de5b34a66fb77c2612d2
PaulMick <paulwmick@gmail.com>
2024-09-14 22:29:58 -0400

    added id rejection

--- track_aruco.py
+++ track_aruco.py
@@ -16,6 +16,9 @@ settings = json.load(in_file)
 marker_length = settings["marker_settings"]["marker_length"]
 matrix_coefs = np.matrix(settings["camera_settings"]["matrix_coefficients"])
 distortion_coefs = np.matrix(settings["camera_settings"]["distortion_coefficients"])
+valid_marker_ids = [settings["marker_settings"]["marker_of_interest_id"]]
+for waymarker in settings["marker_settings"]["waypoint_markers"]:
+    valid_marker_ids.append(waymarker["id"])
 
 cam = cv2.VideoCapture(0)
 
@@ -26,34 +29,31 @@ def findArucoMarkers(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     corners, ids, rejected = aruco.detectMarkers(gray, aruco_dict, parameters = aruco_params)
 
-    # if ids == None:
-    #     print(ids)
-    # else:
-    #     print(ids[0][0])
-
     aruco.drawDetectedMarkers(frame, corners)
 
     return [corners, ids]
 
-def estimateMarkerPoses(corners, ids):
+def estimateMarkerPoses(frame, corners, ids):
     pose_estimations = []
     if len(corners) > 0:
         for i in range(len(ids)):
-            rvec, tvec, marker_points = aruco.estimatePoseSingleMarkers(corners[i], marker_length, matrix_coefs, distortion_coefs)
-            # print(f"rvec: {rvec}, tvec: {tvec}, marker_points: {marker_points}")
-            print(f"Dist z: {tvec[0][0][2]}")
-            pose_estimations.append({
-                "id": int(ids[i][0]),
-                "trans_vec": list(tvec[0][0]),
-                "rot_vec": list(rvec[0][0])
-            })
+            if ids[i][0] in valid_marker_ids:
+                rvec, tvec, marker_points = aruco.estimatePoseSingleMarkers(corners[i], marker_length, matrix_coefs, distortion_coefs)
+                # print(f"rvec: {rvec}, tvec: {tvec}, marker_points: {marker_points}")
+                print(f"[{(ids[i][0])}] Dist z: {tvec[0][0][2]}")
+                pose_estimations.append({
+                    "id": int(ids[i][0]),
+                    "trans_vec": list(tvec[0][0]),
+                    "rot_vec": list(rvec[0][0])
+                })
+                cv2.drawFrameAxes(frame, matrix_coefs, distortion_coefs, rvec, tvec, marker_length)
     return pose_estimations
 
 while True:
     ret, frame = cam.read()
 
     detections = findArucoMarkers(frame)
-    marker_poses = estimateMarkerPoses(detections[0], detections[1])
+    marker_poses = estimateMarkerPoses(frame, detections[0], detections[1])
     # print(marker_poses)
 
     cv2.imshow("Feed", frame)
