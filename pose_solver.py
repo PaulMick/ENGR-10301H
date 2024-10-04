8db08548774a66d9bb714b86c0cb76afa5c3c5fe
PaulMick <paulwmick@gmail.com>
2024-10-04 15:47:44 -0400

    working axis plotter

--- pose_solver.py
+++ pose_solver.py
@@ -21,31 +21,29 @@ def get_rot_matrix(rot_vec: list[float]) -> np.matrix:
     x_mat[1, 2] = math.sin(rot_vec[0])
     x_mat[2, 2] = math.cos(rot_vec[0])
     x_mat[2, 1] = -math.sin(rot_vec[0])
-    # print(x_mat)
 
     y_mat = np.identity(4)
     y_mat[0, 0] = math.cos(rot_vec[1])
     y_mat[2, 0] = math.sin(rot_vec[1])
     y_mat[2, 2] = math.cos(rot_vec[1])
     y_mat[0, 2] = -math.sin(rot_vec[1])
-    # print(y_mat)
 
     z_mat = np.identity(4)
     z_mat[0, 0] = math.cos(rot_vec[2])
     z_mat[0, 1] = math.sin(rot_vec[2])
     z_mat[1, 1] = math.cos(rot_vec[2])
     z_mat[1, 0] = -math.sin(rot_vec[2])
-    # print(z_mat)
 
     return np.matmul(np.matmul(x_mat, y_mat), z_mat)
 
 def get_transformation_matrix(pose: list[float]) -> np.matrix:
     return np.matmul(get_trans_matrix(pose[:3:]), get_rot_matrix(pose[3:6:]))
 
+def get_inverse_transformation_matrix(pose: list[float]) -> np.matrix:
+    return np.linalg.inv(np.matmul(get_trans_matrix(pose[:3:]), get_rot_matrix(pose[3:6:])))
+
 def get_cam_pose_rel_world(marker_in_cam_pose: list[float], marker_in_world_pose: list[float]) -> list[float]:
-    world_to_marker_mat = get_transformation_matrix(marker_in_world_pose)
-    # print(world_to_marker_mat)
-    marker_to_world_mat = np.linalg.inv(world_to_marker_mat)
+    marker_to_world_mat = get_inverse_transformation_matrix(marker_in_world_pose)
 
     cam_to_marker_mat = get_transformation_matrix(marker_in_cam_pose)
 
--- test.py
+++ test.py
@@ -1,14 +1,41 @@
 import pose_solver
 import numpy as np
 from math import pi
+import matplotlib.pyplot as plt
+import time
 
-marker_in_world_pose = [0, 0, 0, 0, 0, 0]
+def get_point(off_set_point: list[float], transform_mat: np.matrix) -> list[float]:
+    off_set_point.append(1)
+    out = np.matmul(off_set_point, transform_mat)
+    return [float(out[0]), float(out[1]), float(out[2])]
+
+def plot_axis(transform_mat: np.matrix):
+    origin = get_point([0, 0, 0], transform_mat)
+    x_points = []
+    x_points.append(origin)
+    x_points.append(get_point([1, 0, 0], transform_mat))
+    plot_3d.plot3D([p[0] for p in x_points], [p[1] for p in x_points], [p[2] for p in x_points], "red")
+    y_points = []
+    y_points.append(origin)
+    y_points.append(get_point([0, 1, 0], transform_mat))
+    plot_3d.plot3D([p[0] for p in y_points], [p[1] for p in y_points], [p[2] for p in y_points], "blue")
+    z_points = []
+    z_points.append(origin)
+    z_points.append(get_point([0, 0, 1], transform_mat))
+    plot_3d.plot3D([p[0] for p in z_points], [p[1] for p in z_points], [p[2] for p in z_points], "green")
+
+plot_3d = plt.axes(projection = "3d")
+
+marker_in_world_pose = [1, 2, 3, 0, 0, 0]
 marker_in_cam_pose = [1, 5, -2, pi / 2, 0, 0]
 
+marker_to_world_mat = pose_solver.get_inverse_transformation_matrix(marker_in_world_pose)
 cam_to_world_mat = pose_solver.get_cam_pose_rel_world(marker_in_cam_pose, marker_in_world_pose)
 
 point = np.matrix([0, 0, 0, 1])
 
-# print(point)
+world_point = np.matmul(point, cam_to_world_mat)
 
-print(np.matmul(point, cam_to_world_mat))
+plot_axis(marker_to_world_mat)
+
+plt.show()
